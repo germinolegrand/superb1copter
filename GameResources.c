572c40aee6744fa2370e3f700073b9d817df49d8
Yanis Cernesse <mryodatak@gmail.com>
2013-05-14 01:55:15 +0200

    Add Ressources
    Tentative de rewite engine du son en prevision

--- GameResources.c
+++ GameResources.c
@@ -4,21 +4,21 @@ void loadResources(GameResources *res)
 {
     res->font = TTF_OpenFont("arial.ttf", 20);
 
-    res->helicoL = SDL_LoadBMP("helicopterL.bmp");
-    res->helicoR = SDL_LoadBMP("helicopterR.bmp");
-    res->base = SDL_LoadBMP("base.bmp");
-    res->building = SDL_LoadBMP("building.bmp");
-    res->buildingD = SDL_LoadBMP("buildingD.bmp");
-    res->bomb = SDL_LoadBMP("bomb.bmp");
-    res->hostage = SDL_LoadBMP("hostage.bmp");
+    res->helicoL = SDL_LoadBMP("Images/copterL.bmp");
+    res->helicoR = SDL_LoadBMP("Images/copterR.bmp");
+    res->base = SDL_LoadBMP("Images/Hospital.bmp");
+    res->building = SDL_LoadBMP("Images/buld.bmp");
+    res->buildingD = SDL_LoadBMP("Images/buldD.bmp");
+    res->bomb = SDL_LoadBMP("Images/bomb.bmp");
+    res->hostage = SDL_LoadBMP("Images/hostagee.bmp");
 
-    SDL_SetColorKey(res->helicoL, SDL_SRCCOLORKEY, SDL_MapRGB(res->helicoL->format, 255, 0, 255));
-    SDL_SetColorKey(res->helicoR, SDL_SRCCOLORKEY, SDL_MapRGB(res->helicoR->format, 255, 0, 255));
-    SDL_SetColorKey(res->base, SDL_SRCCOLORKEY, SDL_MapRGB(res->base->format, 255, 0, 255));
-    SDL_SetColorKey(res->building, SDL_SRCCOLORKEY, SDL_MapRGB(res->building->format, 255, 0, 255));
-    SDL_SetColorKey(res->buildingD, SDL_SRCCOLORKEY, SDL_MapRGB(res->buildingD->format, 255, 0, 255));
-    SDL_SetColorKey(res->bomb, SDL_SRCCOLORKEY, SDL_MapRGB(res->bomb->format, 255, 0, 255));
-    SDL_SetColorKey(res->hostage, SDL_SRCCOLORKEY, SDL_MapRGB(res->hostage->format, 255, 0, 255));
+    SDL_SetColorKey(res->helicoL, SDL_SRCCOLORKEY, SDL_MapRGB(res->helicoL->format, 0, 0, 0));
+    SDL_SetColorKey(res->helicoR, SDL_SRCCOLORKEY, SDL_MapRGB(res->helicoR->format, 0, 0, 0));
+    SDL_SetColorKey(res->base, SDL_SRCCOLORKEY, SDL_MapRGB(res->base->format, 0, 0, 0));
+    SDL_SetColorKey(res->building, SDL_SRCCOLORKEY, SDL_MapRGB(res->building->format, 0, 0, 0));
+    SDL_SetColorKey(res->buildingD, SDL_SRCCOLORKEY, SDL_MapRGB(res->buildingD->format, 0, 0, 0));
+    SDL_SetColorKey(res->bomb, SDL_SRCCOLORKEY, SDL_MapRGB(res->bomb->format, 0, 0, 0));
+    SDL_SetColorKey(res->hostage, SDL_SRCCOLORKEY, SDL_MapRGB(res->hostage->format, 0, 0, 0));
 }
 
 void freeResources(GameResources *res)
