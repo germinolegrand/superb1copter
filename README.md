453033c9303c438ccfd3544daf3d812aaca7c841
Yanis Cernesse <mryodatak@gmail.com>
2013-05-14 04:00:42 +0200

    Son Fonctionnel
    Tri des ressources,FMOD

--- README.md
+++ README.md
@@ -5,7 +5,33 @@ Florian Faure 162400
 Sabil Djerir 159668
 
 =============
-Contr�les du jeu :
+Controles du jeu :
 Alt+F4/croix rouge - quitter le jeu
 P - mettre en pause/reprendre
-<v^> - diriger l'h�licopt�re
+<v^> - diriger l'hélicopt�re
+
+
+Projet développement B1
+
+
+Membres du groupe : 
+Florian Faure 162400
+Florent Chapelle 153732
+Sabil Djerir 159668
+
+Planning prévisionnel : 
+
+Fonctionnalités principales du jeu 
+Intégration d’objets graphiques (images, animations, etc.)
+Gestion des collisions
+Implémentation d’une IA (Intelligence Artificielle) pour les ennemis et les otages 
+Intégration de sons 
+Intégration d’une musique
+Gestion du score
+Ajout d’un menu
+
+--> DONE
+Apprentissage du langage C 
+Apprentissage de la bibliothèque SDL
+Intégration d’une pause
+
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
+#include "fmod.h"
 
 #include "GameResources.h"
 #include "GameShow.h"
@@ -30,6 +31,37 @@ int main(int argc, char* argv[])
     loadResources(&gResources);
     initGame(&gControl);
     loadLevel(1, &gControl);
+
+	//Intialisation de Fmod
+	FMOD_SYSTEM *system;
+    FMOD_SOUND *musique;
+    FMOD_RESULT resultat;
+
+	FMOD_System_Create(&system);
+    FMOD_System_Init(system, 1, FMOD_INIT_NORMAL, NULL);
+
+	/* On ouvre la musique */
+    resultat = FMOD_System_CreateSound(system, "Ressources/Son/tro.xm", FMOD_SOFTWARE | FMOD_2D | FMOD_CREATESTREAM, 0, &musique);
+
+    /* On vérifie si elle a bien été ouverte (IMPORTANT) */
+    if (resultat != FMOD_OK)
+    {
+        fprintf(stderr, "Impossible de lire le fichier mp3\n");
+        return 3;
+    }
+
+    /* On active la répétition de la musique à l'infini */
+    FMOD_Sound_SetLoopCount(musique, -1);
+
+	/* On joue la musique */
+    FMOD_System_PlaySound(system, FMOD_CHANNEL_FREE, musique, 0, NULL);
+
+	FMOD_CHANNELGROUP *canal;
+	FMOD_BOOL etat;
+	FMOD_System_GetMasterChannelGroup(system, &canal);
+	FMOD_ChannelGroup_GetPaused(canal, &etat);
+
+
 
     while(running)
     {
